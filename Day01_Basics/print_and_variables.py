32220a052bf3a9ef52563000531d11243c76644e
rushaco <153447520+rushaco@users.noreply.github.com>
2026-01-10 12:23:27 +0530

    Create print_and_variables.py

---
+++ Day01_Basics/print_and_variables.py
@@ -0,0 +1,17 @@
+# Day 1: Python Basics
+# Author: Rushant Burde
+
+print("array")
+
+print("My name is Rushant, I am learning Python")
+
+a = "Rushant"
+b = "Pune"
+c = "Python"
+
+print("My name is", a, "I live in", b, "I learn", c)
+print(f"My name is {a}, I live in {b}, I learn {c}")
+
+
+Day 1: Basic print statements and variables
+
